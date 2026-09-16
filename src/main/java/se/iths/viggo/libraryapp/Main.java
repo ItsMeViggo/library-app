281f2f703ade157e943e934285f23d465baa723f
ItsMeViggo <viggo.ofors@gmail.com>
2026-09-16 13:21:30 +0200

    Login mechanism created.

--- src/main/java/se/iths/viggo/libraryapp/Main.java
+++ src/main/java/se/iths/viggo/libraryapp/Main.java
@@ -5,15 +5,48 @@ import se.iths.viggo.libraryapp.model.Book;
 import se.iths.viggo.libraryapp.model.Borrower;
 import se.iths.viggo.libraryapp.model.Loan;
 
-public class Main {
-    static void main() {
-        Author author = new Author("JK Rowling");
-        Book book = new Book("Harry Potter", "111123123", author);
-        Borrower borrower = new Borrower("Kalle Johnasson", "KKR#¤%", "KallEJ");
-        Loan loan = new Loan(borrower, book);
+import java.util.ArrayList;
+import java.util.List;
 
-        IO.println(book);
-        IO.println(borrower);
-        IO.println(loan);
+public class Main {
+    static List<Borrower> borrowers = new ArrayList<>();
+    static List<Book> books = new ArrayList<>();
+    static List<Loan> loans = new ArrayList<>();
+
+    static void main() {
+
+
+        borrowers.add(new Borrower("Anna Svensson", "anna123!", "AnnaS"));
+        borrowers.add(new Borrower("Erik Nilsson", "Erik456!", "ErikN"));
+
+        books.add(new Book("Bröderna Lejonhjärta", "12345678", new Author("Astrid Lindgren")));
+        books.add(new Book("1984", "87654321", new Author("George Owell")));
+        books.add(new Book("The hobbit", "13579245", new Author("JRR Tolkien")));
+
+        loans.add(new Loan(borrowers.getFirst(), books.getFirst()));
+        loans.add(new Loan(borrowers.getLast(), books.getLast()));
+
+        login();
+    }
+
+    private static void login() {
+        String username = IO.readln("Enter your username: ");
+        String password = IO.readln("Enter your password: ");
+
+        Borrower currentBorrower = null;
+
+        for (Borrower borrower : borrowers) {
+            if (borrower.getUsername().equals(username)
+                    && borrower.getPassword().equals(password)) {
+                currentBorrower = borrower;
+                break;
+            }
+        }
+
+        if (currentBorrower != null) {
+            IO.println("Login Success!");
+        } else {
+            IO.println("Login Failed.");
+        }
     }
 }
